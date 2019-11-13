ee938059af35be7eaf44f38194cbe4a94467a3ce
anthony Ciszowski <45790380+Ciszowski@users.noreply.github.com>
2019-11-13 11:31:31 +0100

    delete mysql.js

--- Back/database/mysql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '@Toulouse84',
-    database: 'allbikes'
-})
-
-module.exports = connection
